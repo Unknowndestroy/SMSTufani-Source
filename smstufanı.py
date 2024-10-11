2f536f7ee914401cc67927d847951526ebe6fcd6
Unknowndestroy <149819513+Unknowndestroy@users.noreply.github.com>
2024-10-11 23:53:25 +0300

    🥵Güncellendi🥵

--- smstufanı.py
+++ smstufanı.py
@@ -1,14 +1,30 @@
+import time
 from colorama import Fore, Style
 from time import sleep
 from os import system
 from sms import SendSms
 from concurrent.futures import ThreadPoolExecutor, wait
 
-def slow_print(text, delay=0.01, end='\n'):
+
+def slow_print(text, delay=0.008, end='\n'):
     for char in text:
         print(char, end='', flush=True)
         sleep(delay)
     print(end, flush=True)  # Yeni satıra geç veya belirtilen end karakterini yaz
+
+def slow_print_more(text, delay=0.03, end='\n'):
+    for char in text:
+        print(char, end='', flush=True)
+        sleep(delay)
+    print(end, flush=True)  # Yeni satıra geç veya belirtilen end karakterini yaz
+
+
+def slow_print_more_more(text, delay=0.06, end='\n'):
+    for char in text:
+        print(char, end='', flush=True)
+        sleep(delay)
+    print(end, flush=True)  # Yeni satıra geç veya belirtilen end karakterini yaz
+
 
 servisler_sms = []
 for attribute in dir(SendSms):
@@ -16,6 +32,18 @@ for attribute in dir(SendSms):
     if callable(attribute_value):
         if attribute.startswith('__') == False:
             servisler_sms.append(attribute)
+
+
+print(Fore.LIGHTBLUE_EX + """
+© 2024 SMS Tufanı. Tüm Hakları Saklıdır.
+
+Bu yazılım "SMS Tufanı" adı altında geliştirilmiş olup, yalnızca eğitim ve siber güvenlik bilincini artırma amaçlı kullanılmak üzere tasarlanmıştır. Yazılımın ticari kullanım, kötüye kullanım veya herhangi bir yasadışı faaliyet için kullanılması yasaktır. Yazılımın kodu, tasarımı ve işleyişi üzerinde tüm haklar geliştiriciye aittir ve bu yazılımın herhangi bir kısmı, izin alınmaksızın kopyalanamaz, dağıtılamaz veya yeniden üretilemez.
+
+Yazılımın kullanımından doğabilecek yasal, teknik veya kişisel sorunlardan tamamen kullanıcı sorumludur ve geliştirici bu gibi durumlarda hiçbir sorumluluk kabul etmez. Yazılımın kullanımı sırasında ya da sonrasında meydana gelebilecek zararlardan geliştirici sorumlu tutulamaz.
+
+Bu yazılımı kullanarak, belirtilen koşulları kabul etmiş sayılırsınız.
+""")
+time.sleep(5)
 
 while 1:
     system("cls||clear")
@@ -30,7 +58,22 @@ while 1:
     $$    $$/ $$ | $/  $$ |$$    $$/    $$ |   $$    $$/ $$ |     $$ |  $$ |$$ | $$$ |/ $$   |
      $$$$$$/  $$/      $$/  $$$$$$/     $$/     $$$$$$/  $$/      $$/   $$/ $$/   $$/ $$$$$$/ 
                                                                                            {Style.RESET_ALL}by {Fore.LIGHTRED_EX}tiktok.com/@unknown_napim\n
+   """)
+    slow_print_more_more(f"""
+{Style.RESET_ALL}{Fore.LIGHTRED_EX}                                                           UYARI
+""")
+    slow_print_more(f"""
+{Style.RESET_ALL}{Fore.LIGHTRED_EX}Bu yazılım yalnızca eğitim amaçlı olarak geliştirilmiştir ve kullanıcıların bu aracı kötüye kullanması veya herhangi bir kişi, kurum ya da kuruluşa zarar verme amacıyla kullanması kesinlikle yasaktır. Yazılımın amacı, siber güvenlik konusunda farkındalık oluşturmak ve eğitimsel amaçlara hizmet etmektir. Bu aracın kötüye kullanımı sonucu ortaya çıkabilecek herhangi bir hukuki, maddi, manevi veya teknik sorunun tamamıyla kullanıcının kendi sorumluluğunda olduğunu ve geliştirici olarak, bu yazılımın kullanımı sırasında veya sonrasında meydana gelebilecek herhangi bir veri kaybı, sistem arızası, hukuki yaptırım, kişisel zarar ya da üçüncü şahıslara verilebilecek her türlü zarardan sorumluluk kabul etmediğimi beyan ederim. Yazılımın amacı dışında kullanımı, yürürlükteki yasa ve yönetmeliklere aykırıdır ve kullanıcı, bu sorumlulukları ihlal etmesi durumunda her türlü hukuki ve cezai yaptırıma tabi olduğunu kabul eder.
+{Style.RESET_ALL}{Fore.LIGHTRED_EX}Bu kodda aşağıda belirtilen markaların siteleri kullanılmıştır: 
+{Style.RESET_ALL}{Fore.LIGHTRED_EX}Akasya, Akbatı, Ayyıldız, Baydöner, Beefull, Bim, Bisu, Bodrum, Clickme, 
+Domino's, English Home, Evidea, File, Frink, Happy, Hayatsu, Hey, Hızlı Eczane, ICQ, İstegelse, Jojow, Karşıyaka, 
+Konfor, Kozmos, Koton, Limon, MG, Migros, Momo, More, N11, Pizza Hut, Rafik, Ramstore, Simferpol, Slipknot, 
+Turkcell, Uyku, Vivense, Toplam: 49 \n
     """)
+
+
+
+
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Turbo)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
         if menu == "":
